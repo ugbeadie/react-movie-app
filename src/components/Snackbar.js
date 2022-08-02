cab6af7076fd4ae431c522d3df7759f522e913a0
ugbeadie <ugbeadie3@gmail.com>
2022-08-02 17:14:58 +0100

    change snackbar color and background color

--- src/components/Snackbar.js
+++ src/components/Snackbar.js
@@ -10,14 +10,14 @@ const Snackbar = forwardRef((props, ref) => {
             setShowSnackbar(true)
             setTimeout(() => {
                 setShowSnackbar(false)
-            }, 2000);
+            }, 1500);
         }
     }))
 
     return (
         <div className="snackbar"
         id={showSnackbar ? 'show' : 'hide'}
-        style={{backgroundColor: '#00F593'}}>
+        style={{backgroundColor: '#3f4370'}}>
             <div className="symbol"><h1>&#x2713;</h1></div>
             <div className="message">movie added to favorite</div>
         </div>
